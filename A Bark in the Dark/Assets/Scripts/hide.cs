f861f793e07de31de8925d2084728153c9949199
kathermf <kathermf@uci.edu>
2017-05-13 22:47:14 -0700

    Added my test stuff and started hiding mechanic

---
+++ A Bark in the Dark/Assets/Scripts/hide.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour {
+
+    public bool hidden = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("hideyhole")) 
+        {
+            Debug.Log("hidden");
+            hidden = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("hideyhole"))
+        {
+            Debug.Log("not hidden");
+            hidden = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
---
+++ A Bark in the Dark/Assets/Scripts/movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public bool can_move = true;
+    public float speed = 0.04f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
+
+        if (Input.GetKey(KeyCode.W) && can_move)
+        {
+            transform.position = new Vector3(pos.x, pos.y + speed, pos.z);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S) && can_move)
+        {
+            transform.position = new Vector3(pos.x, pos.y - speed, pos.z);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+        if (Input.GetKey(KeyCode.A) && can_move)
+        {
+            transform.position = new Vector3(pos.x - speed, pos.y, pos.z);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        if (Input.GetKey(KeyCode.D) && can_move)
+        {
+            transform.position = new Vector3(pos.x + speed, pos.y, pos.z);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+        }
+
+	}
+}
